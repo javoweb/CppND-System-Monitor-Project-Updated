622119a0a17f0aa15ded82cc698790ce3406a998
Gonzalo Tixilima <javoweb@msn.com>
2020-03-25 16:50:05 -0500

    added helper "Format::ElapsedTime" function

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,17 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    long sec = seconds % 60;
+    long minutes = (seconds - sec) / 60;
+    long min = minutes % 60;
+    long hours = (minutes - min) / 60;
+    string formated_str;
+    if (hours < 10) { formated_str += "0";}
+    formated_str += std::to_string(hours) + ":";
+    if (min < 10) {formated_str += "0";}
+    formated_str += std::to_string(min) + ":";
+    if (sec < 10) {formated_str += "0";}
+    formated_str += std::to_string(sec);
+    return formated_str; 
+}
