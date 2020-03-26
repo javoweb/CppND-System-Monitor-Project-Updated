4ba44a909684da2ac4f956dc353180bf1ef18459
Gonzalo Tixilima <javoweb@msn.com>
2020-03-25 23:24:42 -0500

    OS related task done

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,14 +1,14 @@
 #include "processor.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { 
-    float utilization{0.0};
-    float total = float(LinuxParser::Jiffies());
-    float active = float(LinuxParser::ActiveJiffies());
-    // Calculate utilization rate
-    utilization = (active - prevActive) / (total - prevTotal);
-    // Set current state as previous state for next calculation
-    prevTotal = total;
-    prevActive = active;
-    return utilization;
- }
+float Processor::Utilization() {
+  float utilization{0.0};
+  float total = float(LinuxParser::Jiffies());
+  float active = float(LinuxParser::ActiveJiffies());
+  // Calculate utilization rate
+  utilization = (active - prevActive) / (total - prevTotal);
+  // Set current state as previous state for next calculation
+  prevTotal = total;
+  prevActive = active;
+  return utilization;
+}
